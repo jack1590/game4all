61f99971b5958d450bc846c91546a60b92a6a23a
juan_joya <jcarl.30@gmail.com>
2019-07-09 16:59:55 -0500

    First project setup

---
+++ game4all/src/main/java/app/game4all/Game4allApplication.java
@@ -0,0 +1,13 @@
+package app.game4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Game4allApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Game4allApplication.class, args);
+	}
+
+}
---
+++ game4all/src/test/java/app/game4all/Game4allApplicationTests.java
@@ -0,0 +1,13 @@
+package app.game4all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Game4allApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
